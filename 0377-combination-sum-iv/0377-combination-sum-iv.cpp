b5f2986f68cdd16abc2285267dd0cf7a3713a1ee
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2023-07-27 21:49:24 +0530

    Time: 3 ms (48.81%), Space: 6.6 MB (79.17%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,22 @@
+int solve(vector<int> &arr,int target,vector<int> &dp){
+    if(target==0){       // :) 
+        return 1;           // implementation -_-
+    }
+    if(dp[target]!=-1){
+        return dp[target];
+    }
+    dp[target]=0;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]<=target){
+            dp[target]=dp[target]+solve(arr,target-arr[i],dp);
+        }
+    }
+    return dp[target];
+}
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1,-1);
+        return solve(nums,target,dp);
+    }
+};
