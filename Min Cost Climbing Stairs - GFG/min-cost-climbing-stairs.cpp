3518660d209fcc71794d0d4144957ad5227ac183
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2023-08-13 15:52:38 +0530

    Added solution - LeetHub

---
+++ Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int minCostClimbingStairs(vector<int>&cost ,int n) {
+        //Write your code here
+        vector<int> dp(n+1,-1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        }
+        return min(dp[n-1],dp[n-2]);      //  we should check min of last two values....since last before element can go 2steps easily...it may not
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>cost(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>cost[i];
+        }
+
+        Solution ob;
+        cout<<ob.minCostClimbingStairs(cost,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
