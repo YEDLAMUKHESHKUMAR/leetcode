792a84017dfa4ca19c91be3ffdbfa07800007da6
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2023-09-06 18:01:08 +0530

    Added solution - LeetHub

---
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+// bool solve(int i,int target,int n,vector<int> &arr,vector<vector<int>> &dp){
+//     if(i==n){
+//         if(target==0){                          // memoization + recursion
+//             return true;
+//         }
+//         return false;
+//     }
+//     if(target==0) return true;
+//     if(dp[i][target]!=-1){
+//         return dp[i][target];
+//     }
+//     if(target>=arr[i]){
+//         if(solve(i+1,target-arr[i],n,arr,dp)==true) return dp[i][target]=true;
+//     } 
+
+//         if(solve(i+1,target,n,arr,dp)==true) return dp[i][target]=true;
+//         return dp[i][target]=false;
+//     // if(pick==true || notPick==true) return true;
+//     // return false;
+    
+// }
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        int n=arr.size();
+        vector<vector<bool>> dp(n,vector<bool> (sum+1,0)); // why i used 2d array?? observe there can be different possibility targets with same index ....if you return false without checking various targets you will get false
+        // return solve(0,sum,n,arr,dp);        
+        for(int i=0;i<n;i++){                   // tabulation
+            dp[i][0]=true;                                    
+        }
+        dp[0][arr[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=sum;j++){
+                bool notTake=dp[i-1][j];
+                bool take=false;
+                if(arr[i]<=j){
+                    take=dp[i-1][j-arr[i]];
+                }
+                dp[i][j]=take | notTake;
+            }
+            
+        }
+        return dp[n-1][sum];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
