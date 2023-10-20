05bafe40c81523ccc6137a84ab401e60e6e0bea7
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2023-10-20 22:26:54 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (33.71%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string s="";
+        for(int i=1;i<=n-1;i++){
+            s+='a';
+        }
+        if(n % 2 == 0){
+            s+='b';
+        }
+        else{
+            s+='a';
+        }
+        return s;
+    }
+};
