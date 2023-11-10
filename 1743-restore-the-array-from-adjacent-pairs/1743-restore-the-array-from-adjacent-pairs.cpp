19d50f9eeacab87f07417e00d1229241da355082
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2023-11-10 10:19:46 +0530

    Time: 349 ms (78.79%), Space: 106.5 MB (81.21%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int,vector<int>> mp;
+        int n = adjacentPairs.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++){                      // boogie woogie :)
+            int curr1 = adjacentPairs[i][0];
+            int curr2 = adjacentPairs[i][1];
+            mp[curr1].push_back(curr2);
+            mp[curr2].push_back(curr1);
+        }
+        int startWith ;
+        int GoOn ;
+        for(auto i:mp){
+            if(i.second.size()==1){
+                startWith = i.first;
+                break;
+            }
+        }
+        int j = 0;
+        ans.push_back(startWith);
+        startWith = mp[startWith][0];
+        ans.push_back(startWith);
+        if(n==1) return ans;
+        j+=2;
+        int size1 = 0 ;
+        while(size1 !=2){
+            int currSize = mp[startWith].size();
+            if(currSize==1) size1++;
+            for(int i=0;i<currSize;i++){
+                int currElement = mp[startWith][i];
+                if(currElement!=ans[j-2]){
+                    startWith = currElement ; 
+                    ans.push_back(startWith);
+                    j++;
+                    break;
+                }
+            }
+
+
+        }
+        return ans;
+
+        
+    }
+};
