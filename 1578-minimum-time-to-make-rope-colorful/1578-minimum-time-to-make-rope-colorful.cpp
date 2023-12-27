471a82c8a84841b23772c52a288ebbd860a0d770
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2023-12-27 11:29:40 +0530

    Time: 108 ms (86.89%), Space: 95.8 MB (49.48%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,30 +1,33 @@
-void do_swap(vector<int>& neededTime,int i){
-    int temp=neededTime[i];
-    neededTime[i]=neededTime[i-1];
-    neededTime[i-1]=temp;
-}
-int solve(string colors,vector<int>& neededTime){
-    int min_time=neededTime[0];
-    int n=colors.size();
-    int ans=0;
-    for(int i=1;i<n;i++){
-        int current_color=colors[i];
-        int previous_color=colors[i-1];
-        if(current_color==previous_color){
-            min_time= min(neededTime[i],neededTime[i-1]);
-            ans+=min_time;
-            if(min_time==neededTime[i]){
-                do_swap(neededTime,i);
-            }
-        }
-    
-    }
-    return ans;
-}
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int total_neededTime= solve(colors,neededTime);
-        return total_neededTime;
+        int count = 1;
+        char color = colors[0];
+        int maxi = neededTime[0];  
+        int sum = neededTime[0];
+        int ans = 0;
+        int n = colors.size();
+        for(int i=1;i<n;i++){
+            if(colors[i] == color){
+                count++;
+                sum+=neededTime[i];
+                maxi = max(maxi,neededTime[i]);
+            }
+            else if(colors[i] != color){
+                if(count>1){
+                    sum-= maxi;
+                    ans+=sum;
+                }
+                color = colors[i];
+                maxi = neededTime[i];
+                sum=neededTime[i];
+                count = 1;
+            }
+        }
+        if(count>1){
+            sum-= maxi;
+            ans+=sum;
+        }
+        return ans;
     }
 };
