fa6ac2595a592de300a09841314155d735cc6515
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2024-01-08 00:42:24 +0530

    Time: 401 ms (75.83%), Space: 109.9 MB (73.61%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        int tot_count = 0;
+        vector<unordered_map<int, int>> dp(n);
+
+        for(int i = 1;i<n;i++){
+            for(int j=0;j<i;j++){
+                long long diff = static_cast<long long>(nums[i]) - nums[j]; 
+                if(diff > INT_MAX || diff < INT_MIN) continue;
+                int diff_int = static_cast<int>(diff);  // handeling oferflow
+                dp[i][diff_int] += 1;  
+                // means , we found atleast a subseq >= 2 ;
+                if(dp[j].count(diff_int)){ //if count exist smeans there exists atleast one subseq >=3
+                    dp[i][diff_int] += dp[j][diff_int]; // add valid subseq to i ;
+                    tot_count += dp[j][diff_int] ; 
+                }
+            }
+        }
+        return tot_count;
+    }
+};
