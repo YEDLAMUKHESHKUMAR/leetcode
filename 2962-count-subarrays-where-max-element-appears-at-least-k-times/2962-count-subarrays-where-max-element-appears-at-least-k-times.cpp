fc5cd3bb595103f71abb7d8bb09a395b9d1a9fbe
YEDLAMUKHESHKUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2024-03-28 22:33:12 -0700

    Time: 132 ms (59.11%), Space: 120.1 MB (62.40%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    long long atmost(vector<int> &nums,int k){
-        long long maxi = *max_element(nums.begin(),nums.end());  // :) 
-        int n = nums.size();
-        int j = 0;
+    long long atMost(vector<int> &nums,int k){
+        long long maxi = 0;
         long long ans = 0;
+        int n = nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i] == maxi ) {
-                k--;
-            }
-            while(k<0){
-                if(nums[j] == maxi){
-                    k++;
-                }
+            // maxi = max(maxi,nums[i]);
+            if(nums[i] > maxi) maxi = nums[i];
+        }
+        int j = 0;
+        long long maxCount = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == maxi) maxCount++;
+            while(j<n && maxCount > k){
+                if(nums[j] == maxi) maxCount--;
                 j++;
             }
-            ans+=(i - j  + 1 );
-            
+            ans += i-j+1;
         }
         return ans;
     }
     long long countSubarrays(vector<int>& nums, int k) {
         long long n = nums.size();
-        return (n*(n+1))/2 - atmost(nums,k-1);
+        return (n*(n+1)/2) - atMost(nums,k-1);
+         
     }
 };
