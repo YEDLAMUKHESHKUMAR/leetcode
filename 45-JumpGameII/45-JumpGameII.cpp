47e816b0ae4cbb3b73a62d5c2e6b041f4065d805
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-01-28 22:58:23 +0530

    Update file 1/28/2025, 10:58:26 PM

--- 45-JumpGameII/45-JumpGameII.cpp
+++ 45-JumpGameII/45-JumpGameII.cpp
@@ -15,8 +15,18 @@ public:
     }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return solve(0, nums, dp);
+        // vector<int> dp(n, -1);
+        // return solve(0, nums, dp);
+        vector<int> dp(n , 1e8);
+        dp[n-1] = 0;
+        for(int i = n-1; i>=0;i-- ){
+            for(int j = 1; j<= nums[i]; j++){
+                if(i + j < n){
+                    dp[i] =  min(dp[i], 1 + dp[i + j]);
+                }
+            }
+        }
+        return dp[0];
 
 
     }
