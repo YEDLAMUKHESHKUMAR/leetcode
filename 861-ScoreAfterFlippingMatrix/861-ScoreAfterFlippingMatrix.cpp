24938dd0d94f5a9cc4cef2a5181ddc9509fe9805
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-02-08 00:17:56 +0530

    Create file

---
+++ 861-ScoreAfterFlippingMatrix/861-ScoreAfterFlippingMatrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> flipped(n, 0);
+        for(int i = 0; i<n;i++){
+            if(grid[i][0] == 0){
+                flipped[i] = 1;
+            }
+        }
+        int ans = n * (1 << (m - 1));
+        for(int j = 1; j<m;j++){
+            int z = 0 ;
+            for(int i = 0; i<n;i++){
+                if(flipped[i] == 1){
+                    if(grid[i][j] == 1) z++;
+                }
+                else{
+                    if(grid[i][j] == 0) z++;
+                }
+            }
+            int maxi = max(z, n - z);
+            ans += (maxi * ( 1 << (m - j - 1) ));
+
+        }
+        return ans;
+    }
+};
