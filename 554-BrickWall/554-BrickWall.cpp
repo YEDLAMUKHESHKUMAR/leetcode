cd87e5aadc0996118853868b37182832580d0d10
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-02-18 10:51:53 +0530

    Create file

---
+++ 554-BrickWall/554-BrickWall.cpp
@@ -0,0 +1,42 @@
+#define ll long long
+class Solution {
+public:
+/*
+          1 2 2 1
+          3 1 2
+          1 3 2
+          2 4
+          3 1 2
+          1 3 1 1
+
+
+          1 3 5 6
+          3 4 6
+          1 4 6
+          2 6
+          3 4 6
+          1 4 5 6
+
+
+        */
+
+    int leastBricks(vector<vector<int>>& wall) {
+        
+        int n = wall.size();
+        unordered_map<ll,ll> mp;
+        for(int i = 0; i<n;i++){
+            ll sum = 0;
+            for(int j = 0; j<wall[i].size() - 1;j++){
+                sum += wall[i][j];  // thought of prefix...maybe works ??
+                mp[sum]++;
+            }
+        }
+        ll ans = n;
+        for(auto i : mp){
+            ans = min(ans, n - i.second);
+        }
+        return ans;
+
+
+    }
+};
