97befaff1901ba75742a642ae593be3231266aeb
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-03-23 11:03:12 +0530

    File updated at 3/23/2025, 11:03:12 AM

--- 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.cpp
+++ 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.cpp
@@ -1,8 +1,8 @@
-// Last updated: 3/23/2025, 11:01:10 AM
+// Last updated: 3/23/2025, 11:03:12 AM
+#define ll long long
 class Solution {
 public:
-    int countPaths(int  n, vector<vector<int>>& roads) {
-        // notes
+    int countPaths(int n, vector<vector<int>>& roads) {
         vector<pair<long long,long long>> graph[n];
         long long mod = 1e9 + 7;
         long long m = roads.size(); // notes
@@ -14,32 +14,37 @@ public:
             graph[v].push_back({u,w});
         }
 
-        
-        vector<long long> ways(n);
-        vector<long long> dist(n,LLONG_MAX);
-        priority_queue< pair<long long,long long> , vector<pair<long long,long long>> , greater<pair<long long,long long>> > pq;
-        ways[0] = 1;
+        vector<ll> dist(n, LLONG_MAX);
+        vector<ll> paths(n , 0);
+        priority_queue< pair<ll,ll> , vector<pair<ll,ll>> , greater<pair<ll,ll>> > pq;
+        pq.push({0,0});
+        paths[0] = 1;
         dist[0] = 0;
-        pq.push({0,0}); // {dist,node};
         while(!pq.empty()){
-            long long node = pq.top().second;
-            long long time = pq.top().first;
+            ll time = pq.top().first;
+            ll node = pq.top().second;
             pq.pop();
-            for(auto adj:graph[node]){
-                long long nextNode = adj.first;
-                long long newTime = time + adj.second;
-                if(newTime < dist[nextNode]){
-                    dist[nextNode] = newTime;
-                    ways[nextNode] = 0;
-                    ways[nextNode]  = ( ways[nextNode] +  ways[node] ) % mod ;
-                    pq.push({newTime,nextNode});
+            for(auto adjNode : graph[node]){
+                ll newTime = time + adjNode.second % mod;
+                ll adjacent = adjNode.first;
+                if(newTime < dist[adjacent]){
+                    // if(adjacent == n-1){
+                    //     if(newTime < dist[adjacent]){
+                    //         paths[adjacent] = 0;
+                    //     }
+                    //     paths[n-1] = paths[n-1] + 1 % mod;
+                    // }
+                    dist[adjacent] = newTime ;
+                    paths[adjacent] = 0;
+                    paths[adjacent] = paths[node]  % mod;
+                    pq.push({newTime, adjacent });
                 }
-                else if(newTime == dist[nextNode]){
-                    ways[nextNode]  = ( ways[nextNode] +  ways[node] ) % mod ;
+                else if(newTime == dist[adjacent]){
+                    paths[adjacent] = (paths[adjacent] + paths[node] ) % mod;
                 }
             }
-        } 
-        return ways[n-1] % mod ;
+        }
+        return paths[n-1] % mod;
 
     }
 };
