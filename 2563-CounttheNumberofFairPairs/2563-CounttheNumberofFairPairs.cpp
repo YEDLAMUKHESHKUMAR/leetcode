be7cee0d96df66ec5ed3076136179805a3660194
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-04-19 14:46:19 +0530

    File updated at 4/19/2025, 2:46:17 PM

--- 2563-CounttheNumberofFairPairs/2563-CounttheNumberofFairPairs.cpp
+++ 2563-CounttheNumberofFairPairs/2563-CounttheNumberofFairPairs.cpp
@@ -1,38 +1,49 @@
-// Last updated: 4/19/2025, 2:40:02 PM
+// Last updated: 4/19/2025, 2:46:17 PM
+#define ll long long
 class Solution {
 public:
-    long long getUpperBoundElement(int i, vector<int> &nums, int lower, int upper){
-        long long l = i+1,h = nums.size() - 1;
-        while(l <= h){
-            long long mid = (l + h )/ 2;
-            if(nums[i] + nums[mid] >= lower ){
-                h = mid-1;
+    ll bs(ll tar, vector<int> &nums,ll s,  ll n){
+        ll low = s, high = n - 1, ans = -1;
+        while(low <= high){
+            ll mid = (low + high) >> 1;
+            if(nums[mid] >= tar){
+                ans = mid;
+                high = mid - 1;
             }
-            else l=mid+1;
+            else low = mid + 1;
         }
-        return l;
+        return ans;
+
     }
-    long long getLowerBoundElement(int i, vector<int> &nums, int lower, int upper){
-        long long l = i+1,h = nums.size() - 1;
-        while(l <= h){
-            long long mid = (l + h )/ 2;
-            if(nums[i] + nums[mid] <= upper ){
-                l = mid+1;
+    ll bs2(ll tar, vector<int> &nums ,ll s,  ll n){
+        ll low = s, high = n - 1, ans = -1;
+        while(low <= high){
+            ll mid = (low + high) >> 1;
+            if(nums[mid] <= tar ){
+                ans = mid;
+                low = mid + 1;
             }
-            else h=mid-1;
+            else high = mid - 1;
         }
-        return h;
+        return ans;
     }
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        // 0 1 4 4 5 7  // :)
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        long long ans = 0;
-        for(int i = 0;i<n-1;i++){
-            long long l = getUpperBoundElement(i,nums,lower,upper);
-            long long r = getLowerBoundElement(i,nums,lower,upper);
-            if( l>=n || r < i || ((nums[i] + nums[l] ) > upper) ||  ((nums[i] + nums[r] ) < lower) ) continue;
-            ans += (r - l + 1);
+        // aced in one, go....  :) ...
+        // do this again....
+        // 0 + x <= 6
+        // 0 + x >= 3
+        sort(nums.begin(), nums.end());
+        ll n = nums.size();
+        ll ans = 0;
+        for(int i = 0; i < n ;i++){
+            ll l =  lower - nums[i];
+            ll r = upper - nums[i];
+            ll ind1 = bs(l, nums,i + 1,  n);
+            ll ind2 = bs2(r, nums, i + 1,  n);
+            cout<<"i : "<<i<<"ind1 : "<<ind1<<" "<<ind2<<endl;
+            if(ind1 == -1 || ind2 == -1) continue;
+            if(ind2 >= ind1 && ind2 > i) ans += (ind2 - ind1 + 1);
+            
         }
         return ans;
     }
