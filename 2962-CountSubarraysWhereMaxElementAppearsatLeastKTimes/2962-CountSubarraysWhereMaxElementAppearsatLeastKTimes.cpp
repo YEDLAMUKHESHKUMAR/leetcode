dfe9fd7505a70e53b04295031cd12d3467f4adbb
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-04-29 17:31:43 +0530

    Create file

---
+++ 2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes/2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes.cpp
@@ -0,0 +1,29 @@
+// Last updated: 4/29/2025, 5:31:42 PM
+class Solution {
+public:
+    long long atMost(vector<int> &nums,int k){
+        long long maxi = 0;
+        long long ans = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            // maxi = max(maxi,nums[i]);
+            if(nums[i] > maxi) maxi = nums[i];
+        }
+        int j = 0;
+        long long maxCount = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == maxi) maxCount++;
+            while(j<n && maxCount > k){
+                if(nums[j] == maxi) maxCount--;
+                j++;
+            }
+            ans += i-j+1;
+        }
+        return ans;
+    }
+    long long countSubarrays(vector<int>& nums, int k) {
+        long long n = nums.size();
+        return (n*(n+1)/2) - atMost(nums,k-1);
+         
+    }
+};
