ff3abd47ada869fac9f07043cffbcef182396195
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-06-01 11:27:02 +0530

    File updated at 6/1/2025, 11:27:01 AM

--- 3566-PartitionArrayintoTwoEqualProductSubsets/3566-PartitionArrayintoTwoEqualProductSubsets.cpp
+++ 3566-PartitionArrayintoTwoEqualProductSubsets/3566-PartitionArrayintoTwoEqualProductSubsets.cpp
@@ -1,31 +1,14 @@
-// Last updated: 6/1/2025, 11:22:50 AM
-#define ll long double
+// Last updated: 6/1/2025, 11:27:01 AM
 class Solution {
 public:
-    // bool solve()
     bool checkEqualPartitions(vector<int>& nums, long long target) {
-        // return solve(0, nums, target);
-        int n = nums.size();
-        ll prod = 1;
-        for(int i = 0;  i < n ;i++){
-            prod *= nums[i];
+        int n=nums.size();
+        long long ans=1;int p=0;
+        for(auto i:nums){
+          if(target%i)return 0;
+            
+            
         }
-        for(int i = 0; i < (1 << n) ; i++){
-            ll p = 1, np = 1;
-            for(int pos = 0; pos < 32 ;pos++){
-                if( (i >> pos) & 1 ){
-                    p *= nums[pos];
-                }
-                else if(pos < n ){
-                    np *= nums[pos];
-                }
-            }
-            if(p == target && np == target){
-                // cout<<i;
-                return true;
-            }
-        }
-
-        return false;
+        return 1;
     }
 };
