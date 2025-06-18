a89ea5c1d9c17a0a37ab442e2d0131501f18d486
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-06-18 10:02:48 +0530

    Create 2966-DivideArrayIntoArraysWithMaxDifference

---
+++ 2966-DivideArrayIntoArraysWithMaxDifference/2966-DivideArrayIntoArraysWithMaxDifference.cpp
@@ -0,0 +1,28 @@
+// Last updated: 6/18/2025, 10:02:47 AM
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        int j = 0;
+        int i = j;
+        for(i=0;i<n;i+=3){
+            vector<int> part;
+            part.push_back(nums[i]);
+            j = i+1;
+            int c = 0;
+            while(i<n && c<2 && j<n){
+                int diff = nums[j] - nums[j-1];
+                if(diff > k) return {};
+                part.push_back(nums[j]);
+                j++;
+                c++;
+            }
+            if(part[part.size()-1] - part[0] > k) return {};
+            ans.push_back(part);
+            
+        }
+        return ans;
+    }
+};
