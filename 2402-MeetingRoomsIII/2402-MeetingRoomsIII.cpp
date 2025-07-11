57f7d36f3fb76ff7834ec740ee1604b5dad95985
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-07-11 22:24:02 +0530

    File updated at 7/11/2025, 10:24:01 PM

--- 2402-MeetingRoomsIII/2402-MeetingRoomsIII.cpp
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.cpp
@@ -1,72 +1,52 @@
-// Last updated: 7/11/2025, 10:17:37 PM
-bool compare(vector<int>& v1, vector<int>& v2) {
-    return v1[0] < v2[0];
-}
+// Last updated: 7/11/2025, 10:24:01 PM
+#define ll long long
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        /* Sort the meetings based on start_time */
-        sort(meetings.begin(), meetings.end(), compare);
-        
-        /* Create a priority queue for engaged rooms. Each node of PQ will store {current_meeting_ending_time, room_number} */
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> engaged;
-        
-        /* Create a priority queue for non-engaged rooms. Each node of PQ will store {room_number} */
-        priority_queue<int, vector<int>, greater<int>> unused;
-        
-        /* Frequency map to store the frequency of meetings in each room */
-        unordered_map<int, int> freq;
-        
-        /* Currently all the rooms are mepty */
-        for(int i = 0; i < n; i++) {
-            unused.push(i);
+        // ll m = meetings.size();
+        sort(meetings.begin(), meetings.end() );
+        priority_queue< pair<ll, ll> ,  vector<pair<ll,ll>> , greater<pair<ll,ll>>> used;
+        priority_queue< ll, vector<ll>, greater<ll> > notUsed;
+        unordered_map<ll,ll> mp;
+
+        for(int i = 0; i < n ;i++){
+            notUsed.push(i);
         }
-        
-        for(auto x:meetings) {
-            int s = x[0], e = x[1];
-            
-            /* Move the meeting rooms in engaged, with ending_time <= s, to unused */ 
-            while(engaged.size() > 0 && engaged.top().first <= s) {
-                int room = engaged.top().second;
-                engaged.pop();
-                
-                unused.push(room);
+
+        for(auto meet : meetings){
+            ll st = meet[0] ,end = meet[1];
+
+            while(!used.empty() && used.top().first <= st){
+                ll top = used.top().second;
+                used.pop();
+                notUsed.push(top);
             }
-            
-            /* If there are multiple empty rooms, choose the one with lower room_number */
-            if(unused.size() > 0) 
-            {
-                int room = unused.top();
-                unused.pop();
-                
-                freq[abs(room)] += 1;
-                
-                /* Mark the room as engaged */
-                engaged.push({e, room});
+
+            if(!notUsed.empty()){
+                ll unusedRoom = notUsed.top();
+                notUsed.pop();
+                used.push({ end, unusedRoom });
+                mp[unusedRoom]++;
             }
-            /* If there are no empty rooms, wait for the engaged room with nearest ending time to empty */
-            else 
-            {
-                pair<long long, int> topmost = engaged.top();
-                engaged.pop();
-            
-                freq[abs(topmost.second)] += 1;
-                
-                /* Since duration has to be the same, the newEnd will be sum(end_time_of_the_prev_meeting, duration) */ 
-                long long newEnd = topmost.first;
-                newEnd += (e - s);
-                
-                /* Mark the room as engaged */
-                engaged.push({newEnd, topmost.second});
+            else{
+                ll newSt = used.top().first;
+                ll top = used.top().second;
+                mp[top]++;
+                used.pop();
+                ll newEnd = newSt + (end - st);
+                ll unusedRoom = notUsed.top();
+                used.push({ newEnd, top });
             }
+
+
+
         }
-        
-        /* Find the lowest room_number with maximum frequency */
-        int maxi = 0;
-        for(int i = 1; i < n; i++) {
-            if(freq[i] > freq[maxi])
-                maxi = i;
+
+        ll maxi = 0;
+        for(ll i = 1; i < n; i++) {
+            if(mp[i] > mp[maxi]) maxi = i;
         }
         return maxi;
+
     }
 };
