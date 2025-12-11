13ab412f4463ede3a4b88d9821df76aaf5f0dda3
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2025-12-12 00:26:54 +0530

    Time: 300 ms (5.03%), Space: 12 MB (5.03%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    int isPossible(int mid, vector<int>& nums, int k){
+    int solve(int i, int k, vector<int>& nums, vector<vector<int>>& dp){
         int n = nums.size();
+        if(i == n && k == 0) return 0;
+        if(i >= n && k > 0) return INT_MAX;
+        if(i < n && k == 0) return INT_MAX;
+        if(dp[i][k] != -1) return dp[i][k];
         int sum = 0;
-        k--;
-        for(int i = 0; i < n ;i++){
-            if(sum + nums[i] > mid){
-                sum = nums[i];
-                k--;
-            }
-            else sum += nums[i];
+        int curAns = INT_MAX;
+        for(int ind = i; ind < n ; ind++ ){
+            sum += nums[ind];
+
+            int nextSum = solve(ind + 1, k - 1, nums, dp);
+
+            curAns = min(curAns, max(sum, nextSum));
         }
-        if(k < 0 ) return false;
-        return true;
+        return dp[i][k] = curAns;
+
+
+
     }
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-        int maxi = INT_MIN , sum = 0;
-        for(int i = 0; i < n ;i++){
-            sum += nums[i];
-            maxi = max(maxi, nums[i]);
-        }
-        int low = maxi, high = sum, ans = -1;
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(isPossible(mid, nums, k)){
-                ans = mid;
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
-        }
-        return ans;
+        // int ans = INT_MAX;
+        vector<vector<int>> dp(n + 1, vector<int> (k + 1, -1));
+        return solve(0, k, nums , dp);
     }
 };
