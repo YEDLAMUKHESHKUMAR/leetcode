3e5e6a554b5451ea44fae3f63261daa11305e107
YEDLA MUKHESH KUMAR <116479893+YEDLAMUKHESHKUMAR@users.noreply.github.com>
2026-01-28 00:22:00 +0530

    Time: 2209 ms (5.01%), Space: 36.1 MB (14.38%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        // notes
         int n = beginWord.size();
-        unordered_map<string,int> mp;
-        for(int i = 0;i<wordList.size();i++){
-            mp[wordList[i]]++;
+        int m = endWord.size();
+        // if(n != m) return 0;
+        queue<string> currWords;
+        unordered_map<string, int> containsWord;
+        unordered_map<string, int> vis;
+        for(int i = 0; i < wordList.size(); i++){
+            containsWord[wordList[i]]++;
         }
-        if(mp.find(endWord) == mp.end()) return 0;
-        queue<pair<string,int>> q;
-        q.push({beginWord,0});
-        while(!q.empty()){
-            string word = q.front().first;
-            int level = q.front().second;
-            q.pop();
-            for(int i = 0 ;i < n ;i++){
-                string temp = word;
-                for(int j = 0;j<26;j++){
-                    temp[i] = char( j + 'a');
-                    // cout<<temp<<endl;
-                    if(temp == endWord) return level + 2 ;
-                    if(mp.find(temp) != mp.end()){
-                        // return 32;
-                        q.push({temp,level + 1 });
-                        mp.erase(temp);
+        if(containsWord.find(endWord) == containsWord.end()){
+            return 0;
+        }
+        currWords.push(beginWord);
+        int level = 0;
+        while(!currWords.empty()){
+            level++;
+            int size = currWords.size();
+            for(int i = 0 ; i < size; i++){
+                string word = currWords.front();
+                if(word == endWord) return level;
+                vis[word]++;
+
+                for(int j = 0; j < word.size(); j++){
+                    char tempCurrChar = word[j];
+                    for(char ch = 'a' ; ch <= 'z' ; ch++){
+                        word[j] = ch;
+                        if(vis.find(word) == vis.end() && containsWord.find(word) != containsWord.end()){
+                            currWords.push(word);
+
+                        }
                     }
+                    word[j] = tempCurrChar;
                 }
+
+                currWords.pop();
             }
-            
+
         }
         return 0;
 
+        
+        
     }
 };
